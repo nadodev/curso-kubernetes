c19a6d1445e0e278887d9524a9f1cca9e5ed5c46
leonardo geja <leonardogeja@leonardos-MacBook-Pro.local>
2024-03-17 19:04:45 -0300

    add env object

--- server.go
+++ server.go
@@ -1,12 +1,31 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
 
 func main() {
-   http.HandleFunc("/", Hello)
-   http.ListenAndServe(":9000", nil)
+	http.HandleFunc("/", Hello)
+	http.HandleFunc("/config-map", ConfigMap)
+	http.ListenAndServe(":9000", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-   w.Write([]byte("<h1> Hello Kubernetes</h1>"))
+	name := os.Getenv("NAME")
+	age := os.Getenv("AGE")
+
+	fmt.Fprintf(w, "Hello, %s! You are %s years old. ", name, age)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+	data, err := ioutil.ReadFile("myfamily/family.txt")
+	if err != nil {
+		fmt.Fprintf(w, "Error reading file: %v", err)
+
+	}
+
+	fmt.Fprintf(w, "Data from config map: %s.", string(data))
 }
